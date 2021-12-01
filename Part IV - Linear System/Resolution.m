de25a7aa7933cf7fae1ec06bf0efd492b7f49e64
Serena Tetart <65224852+Serenalyw@users.noreply.github.com>
2021-12-01 19:15:19 +0100

    Add files via upload

---
+++ Part IV - Linear System/Resolution.m
@@ -0,0 +1,17 @@
+function X = Resolution(A, b)
+  
+  n = length(A);
+  X=b;
+  if(A == triu(A))
+    X(n) = b(n)./A(n, n);
+    for i = n-1:-1:1
+      X(i) = (b(i) - A(i, i+1:n)*X(i+1:n))./A(i,i);
+    endfor
+  elseif(A == tril(A))
+    X(1) = b(1)./A(1, 1);
+    for i = 2:n
+      X(i) = (b(i) - A(i, 1:i-1)*X(1:i-1))./A(i,i);
+    endfor
+  endif
+
+endfunction
