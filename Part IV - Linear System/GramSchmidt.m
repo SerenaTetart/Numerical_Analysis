e545a6895a8b1fc03a60c90bb04b2af927c83678
Serena Tetart <65224852+Serenalyw@users.noreply.github.com>
2021-12-05 19:40:11 +0100

    Add files via upload

---
+++ Part IV - Linear System/GramSchmidt.m
@@ -0,0 +1,13 @@
+function e = GramSchmidt(v)
+  
+  n=length(v);
+  e = []; u = [];
+  for k = 1:n
+    u(k, :) = v(k, :)
+    for j  = 1:k-1
+      u(k, :) = u(k, :) - (((u(j, :).*v(k, :))/(u(j, :).*u(j, :)))*u(j, :))
+    endfor
+    e(k, :) = u(k, :)/norm(u(k, :))
+  endfor
+
+endfunction
